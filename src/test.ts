ba36636a64c32ee69c51b8e8870bca2c72deeede
wuqiangnan <2441277210@qq.com>
2021-11-20 22:41:23 +0800

    feat(interview): interview

--- src/test.ts
+++ src/test.ts
@@ -1,124 +1,44 @@
-/* class Modal {
-  static instance: null | HTMLElement;
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = document.createElement("div");
-      this.instance.innerHTML = "我是全局唯一的 modal";
-      this.instance.className = "modal";
-      this.instance.style.display = "none";
-      document.body.appendChild(this.instance);
-    }
+class PreLoadImg {
+  imgNode: HTMLImageElement;
 
-    return this.instance;
+  constructor(imgNode: HTMLImageElement) {
+    this.imgNode = imgNode;
   }
-} */
 
-// const Modal = (function () {
-//   let instance: HTMLElement | null = null;
-//   return () => {
-//     if (!instance) {
-//       instance = document.createElement("div");
-//       instance.className = "modal";
-//       instance.style.display = "none";
-//       instance.innerHTML = "您还未登录哦";
-//       document.body.appendChild(instance);
-//     }
-//     return instance;
-//   };
-// })();
-
-// document.getElementsByClassName("open")[0].addEventListener("click", () => {
-//   openModal();
-//   changeBnStatus();
-// });
-
-// function openModal() {
-//   const modal = Modal();
-//   modal.style.display = "block";
-// }
-
-// document.getElementsByClassName("close")[0].addEventListener("click", () => {
-//   const modal = Modal();
-//   modal.style.display = "none";
-// });
-
-// // 修改按钮文案
-// function changeBtnText() {
-//   const btn = document.getElementsByClassName("open")[0];
-//   btn.innerHTML = "快去登录";
-// }
-
-// // 按钮置灰
-// function disableBtn() {
-//   const btn = document.getElementsByClassName("open")[0];
-//   btn.setAttribute("disabled", "true");
-// }
-
-// // 修改按钮状态
-// function changeBnStatus() {
-//   changeBtnText();
-//   disableBtn();
-// }
-
-class Modal {
-  static instance: HTMLElement | null;
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = document.createElement("div");
-      this.instance.innerHTML = "我是全局唯一的 modal";
-      this.instance.style.display = "none";
-      this.instance.className = "modal";
-      document.body.appendChild(this.instance);
-    }
-    return this.instance;
+  // 设置真实的图片地址
+  setSrc(targetUrl: string) {
+    this.imgNode.src = targetUrl;
   }
 }
 
-// 打开按钮
-class OpenBtn {
-  // 打开 modal
-  onClick() {
-    const modal = Modal.getInstance();
-    modal.style.display = "block";
+class ProxyImg {
+  // 占位图 url 地址
+  static LOADING_IMG = "https://cdn2.thecatapi.com/images/8kq.gif";
+  // 目标Image，即PreLoadImage实例
+  targetImg: PreLoadImg;
+
+  constructor(targetImg: PreLoadImg) {
+    this.targetImg = targetImg;
+  }
+
+  setSrc(targetUrl: string) {
+    // 真实img节点初始化时展示的是一个占位图
+    this.targetImg.setSrc(ProxyImg.LOADING_IMG);
+    // 创建一个帮助加载的 Image 实例
+    const vImage = new Image();
+    // 监听目标图片的加载情况，完成式再将 img 节点的 src 属性设置为目标图片的 url
+    vImage.onload = () => {
+      this.targetImg.setSrc(targetUrl);
+    };
+    // 设置 src 属性，Image 实例开始加载图片
+    setTimeout(() => {
+      vImage.src = targetUrl;
+    }, 3000);
   }
 }
 
-// 定义按钮的装饰着
-class Decorator {
-  openBtn: OpenBtn;
-  constructor(openBtn: OpenBtn) {
-    this.openBtn = openBtn;
-  }
+const imgNode = document.getElementsByTagName("img")[0];
+const preLoadImg = new PreLoadImg(imgNode);
+const proxyImg = new ProxyImg(preLoadImg);
 
-  onClick() {
-    this.openBtn.onClick();
-    this.changeBtnStatus();
-  }
-  // 改变按钮状态
-  changeBtnStatus() {
-    this.changeBtnText();
-    this.disableBtn();
-  }
-  // 按钮置灰
-  disableBtn() {
-    const btn = document.getElementsByClassName("open")[0];
-    btn.setAttribute("disabled", "true");
-  }
-  // 改变按钮文案
-  changeBtnText() {
-    const btn = document.getElementsByClassName("open")[0];
-    btn.innerHTML = "您还未登录哦";
-  }
-}
-
-const openBtn = new OpenBtn();
-const decorator = new Decorator(openBtn);
-
-document.getElementsByClassName("open")[0].addEventListener("click", () => {
-  decorator.onClick();
-});
-
-document.getElementsByClassName("close")[0].addEventListener("click", () => {
-  const modal = Modal.getInstance();
-  modal.style.display = "none";
-});
+proxyImg.setSrc("https://cdn2.thecatapi.com/images/ced.jpg");

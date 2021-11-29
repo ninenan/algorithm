5df166319560765f2e1fce06753e16ec8ddede06
wuqingnan <2441277210@qq.com>
2021-11-29 17:21:56 +0800

    feat: interview

--- src/test.ts
+++ src/test.ts
@@ -101,7 +101,7 @@ function testFn2(event: EventTarget) {
 }
 
 // debounce
-/* const clickTestFn = debounce(testFn2, 5000, true);
+/* const clickTestFn = debounce(testFn2, 3000, true);
 imgNode.addEventListener("click", clickTestFn);
 btn.addEventListener("click", () => clickTestFn.cancel()); */
 
@@ -120,19 +120,150 @@ btn.addEventListener("click", () => clickTestFn.cancel()); */
   };
 }; */
 
-// 时间戳版本
 const throttle = function (fn: Function, delay: number) {
-  let previous = 0;
-
+  let flag = true;
   return function (...reset: unknown[]) {
-    const now = +new Date();
-    if (now - previous > delay) {
-      previous = now;
+    if (!flag) return;
+    if (flag) {
+      flag = false;
+      setTimeout(() => {
+        fn.apply(this, reset);
+        flag = true;
+      }, delay);
+    }
+  };
+};
+
+// 时间戳版本
+const TimestampThrottle = function (fn: Function, delay: number) {
+  let previous = 0;
+  return function (...reset: unknown[]) {
+    const nowTime = +new Date();
+    if (nowTime - previous > delay) {
+      previous = nowTime;
       fn.apply(this, reset);
     }
   };
 };
 
-const clickTestFn = throttle(testFn2, 1000);
+// 时间戳版本
+// const throttle = function (fn: Function, delay: number) {
+//   let previous = 0;
+
+//   return function (...reset: unknown[]) {
+//     const now = +new Date();
+//     if (now - previous > delay) {
+//       previous = now;
+//       fn.apply(this, reset);
+//     }
+//   };
+// };
+
+/* const throttle3 = function (fn: Function, delay: number) {
+  let timeout: number | null = null,
+    previous = 0,
+    args: any | null = null,
+    context: any = null;
+
+  const later = function () {
+    previous = +new Date();
+    timeout = null;
+    fn.apply(context, args);
+  };
+
+  const throttled = function () {
+    const now = +new Date();
+    // 下次触发 fn 的剩余事件
+    const remaining = delay - (now - previous);
+    context = this;
+    args = arguments;
+    // 如果没有剩余时间或者用户修改了系统时间
+    if (remaining <= 0 || remaining > delay) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      fn.apply(context, args);
+    } else if (!timeout) {
+      timeout = setTimeout(later, delay);
+    }
+  };
+  return throttled;
+}; */
+
+interface IOptions {
+  leading?: boolean; // 表示禁用第一次执行
+  trailing?: boolean; // 表示禁用停止触发的回调
+}
+
+/**
+ * 节流
+ * leading：false 和 trailing: false 不能同时设置。
+ * 如果同时设置的话，比如当你将鼠标移出的时候，因为 trailing 设置为 false，停止触发的时候不会设置定时器，
+ * 所以只要再过了设置的时间，再移入的话，就会立刻执行，
+ * 就违反了 leading: false，bug 就出来了，所以，这个 throttle 只有三种用法：
+ * throttle(getUserAction, 1000);
+ * throttle(getUserAction, 1000, {leading: false});
+ * throttle(getUserAction, 1000, {trailing: false});
+ * @param fn 回调函数
+ * @param delay 延迟时间
+ * @param options 选项
+ * @returns 执行回调函数的执行结果
+ */
+const throttle4 = function (
+  fn: Function,
+  delay: number,
+  options: IOptions = { leading: true, trailing: true }
+) {
+  let timeout: number | null = null,
+    context: any = null,
+    previous: number = 0,
+    args: any = null;
+
+  const later = function () {
+    previous = options.leading === false ? 0 : +new Date();
+    timeout = null;
+    fn.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  const throttled = function () {
+    const now = +new Date();
+    if (!previous && options.leading === false) previous = now;
+    const remaining = delay - (now - previous);
+    context = this;
+    args = arguments;
+    // 如果没有剩余时间了或者用户修改了系统时间
+    if (remaining <= 0 || remaining > delay) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      fn.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing) {
+      timeout = setTimeout(later, delay);
+    }
+  };
+
+  /**
+   * 取消
+   */
+  throttled.cancel = function () {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    previous = 0;
+    timeout = null;
+  };
+
+  return throttled;
+};
+
+const clickTestFn = throttle4(testFn2, 3000, {
+  leading: true,
+  trailing: true,
+});
 imgNode.addEventListener("click", clickTestFn);
-// btn.addEventListener("click", () => clickTestFn.cancel());
+btn.addEventListener("click", () => clickTestFn.cancel());

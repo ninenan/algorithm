2e2fc970741d3d80bcfdb23f4e70ec251a5ad53d
wuqiangnan <2441277210@qq.com>
2022-04-16 00:49:44 +0800

    feat(study): algorithm

--- src/swordOffer/19.maxDepth.ts
+++ src/swordOffer/19.maxDepth.ts
@@ -4,3 +4,24 @@ const maxDepth = (root: TreeNode | null): number => {
   if (root === null) return 0;
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
+
+const maxDepth1 = (root: TreeNode | null): number => {
+  if (root === null) return 0;
+  let queue = [],
+    res = 0;
+  queue.push(root);
+
+  while (queue.length) {
+    let len = queue.length;
+
+    for (let index = 0; index < len; index++) {
+      let node = queue.shift();
+
+      if (node?.left) queue.push(node.left);
+      if (node?.right) queue.push(node.right);
+    }
+    res += 1;
+  }
+
+  return res;
+};

7ff758ea80a5b059ea2e942736f7bef5d9bbb2b8
ninenan <wuqingnan.wqn@digital-engine.com>
2022-07-11 16:51:00 +0800

    feat(algorithm): removeElement

--- src/other/27.removeElement.ts
+++ src/other/27.removeElement.ts
@@ -14,3 +14,22 @@ const removeElement = (nums: number[], val: number): number => {
 
   return nums.length;
 }
+
+const removeElement2 = (nums: number[], val: number): number => {
+  const len = nums.length;
+
+  if (!len) {
+    return 0;
+  }
+
+  let start = 0;
+
+  for (let index = 0; index < len; index++) {
+    if (nums[index] !== val) {
+      nums[start] = nums[index];
+      start++;
+    }
+  }
+
+  return start;
+}

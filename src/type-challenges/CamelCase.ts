a70846f863ef620081559d62e6ad889ca5070395
ninenan <wuqingnan.wqn@digital-engine.com>
2022-08-08 11:10:49 +0800

    feat: typechallenge

---
+++ src/type-challenges/CamelCase.ts
@@ -0,0 +1,13 @@
+// CamelCase<StringTypes>
+// 实现 CamelCase <T>，将连字符转驼峰
+
+type CamelCase<T extends string> = T extends `${infer S1}-${infer S2}`
+  ? S2 extends Capitalize<S2>
+    ? `${S1}-${CamelCase<S2>}`
+    : `${S1}${CamelCase<Capitalize<S2>>}`
+  : T;
+
+// T1 = helloWorldWolf
+type T1 = CamelCase<'hello-world-wolf'>;
+
+export {};
---
+++ src/type-challenges/KebabCase.ts
@@ -0,0 +1,14 @@
+// KebabCase<StringTypes>
+// FooBarBaz -> foo-bar-baz
+
+type KebabCase<T extends string> = T extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${S1}-${KebabCase<S2>}`
+  : T;
+
+// T1 = foo-bar-ts
+type T1 = KebabCase<'FooBarTs'>
+type T2 = KebabCase<'HelloWrold'>
+
+export {}

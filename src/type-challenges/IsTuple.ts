90cbd61a0df911fb728e4f79c325324861d0c0b0
ninenan <wuqingnan.wqn@digital-engine.com>
2022-09-01 14:23:20 +0800

    feat: type-challenge

--- src/type-challenges/IsTuple.ts
+++ src/type-challenges/IsTuple.ts
@@ -1,11 +1,19 @@
 // IsTuple<Types>
 // 判断是否是元祖类型
 
-type IsTuple<T> = [T] extends [never] ? false : T extends readonly any[] ? number extends T['length'] ? false : true : false;
+type IsTuple<T> = [T] extends [never]
+  ? false
+  : T extends readonly any[]
+  ? number extends T["length"]
+    ? false
+    : true
+  : false;
 
 // case1 = true
-type case1 = IsTuple<[number]>
+type case1 = IsTuple<[number]>;
 // case2 = true
-type case2 = IsTuple<readonly [number]>
+type case2 = IsTuple<readonly [number]>;
 // case3 = false
-type case3 = IsTuple<number[]>
+type case3 = IsTuple<number[]>;
+
+export {};
--- src/type-challenges/PercentageParser.ts
+++ src/type-challenges/PercentageParser.ts
@@ -1,7 +1,6 @@
 // PercentageParser<StringTypes>
 // 实现类型 PercentageParser。根据规则 /^(\+|\-)?(\d*)?(\%)?$/ 匹配类型 T。
 // 匹配的结果由三部分组成，分别是：[正负号, 数字, 单位]，如果没有匹配，则默认是空字符串。
-
 type Suffix<T extends string> = T extends `${infer S}%` ? [S, "%"] : [T, ""];
 type Prefix<T extends string> = T extends "+" | "-" ? T : never;
 
@@ -23,4 +22,4 @@ type R3 = PercentageParser<PString3> // expected ["-", "85", "%"]
 type R4 = PercentageParser<PString4> // expected ["", "85", "%"]
 type R5 = PercentageParser<PString5> // expected ["", "85", ""]
 
-extends {};
+export {};

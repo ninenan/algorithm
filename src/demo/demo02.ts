1b9cbce48b86744dc6245c51c3fa29f33372e699
wuqiangnan <2441277210@qq.com>
2022-09-22 22:22:09 +0800

    feat: p-limit

--- src/demo/demo02.ts
+++ src/demo/demo02.ts
@@ -1,50 +1,49 @@
-const sleep = (timer: number) => new Promise((resolve) => setTimeout(resolve, timer));
+type Fn = (...args: unknown[]) => Promise<ReturnType> | ReturnType
 
-function promiseLimit(fns = [], maxNum: number): Promise<any> {
-  // 请求总数量
-  const len = fns.length;
-  // 根据请求数量创建一个数组来保存请求的结果
-  const result = new Array(len).fill(false);
-  // 当前完成的数量
-  let count = 0;
+const pLimit = (concurrency: number) => {
+  if (!Number.isInteger(concurrency) && concurrency < 0) {
+    return Promise.resolve();
+  }
+  const queue: any[] = [];
+  let activeCount = 0;
 
-  return new Promise((resolve, reject) => {
-    // 请求maxNum个
-    while (count < maxNum) {
-      next();
+  const next = () => {
+    activeCount--;
+    if (queue.length > 0) {
+      queue.shift()();
     }
-    async function next() {
-      let current = count++;
-      // 处理边界条件
-      if (current >= len) {
-        // 请求全部完成就将promise置为成功状态, 然后将result作为promise值返回
-        !result.includes(false) && resolve(result);
-        return;
+  };
+  const run = async (fn: Fn, resolve, args: unknown[]) => {
+    activeCount++;
+
+    const result = (async () => fn(...args))();
+    resolve(result);
+
+    try {
+      await result;
+    } catch {}
+
+    next();
+  };
+
+  const enqueue = (fn: Fn, resolve,  args: unknown[]) => {
+    queue.push(run.bind(undefined, fn, resolve, args));
+
+    (async () => {
+      await Promise.resolve();
+
+      if (activeCount < concurrency && queue.length > 0) {
+        queue.shift()();
       }
+    })();
+  };
 
-      const fn = fns[current];
-      console.log(`开始 ${current}`, new Date().toLocaleString());
+  const generator = (fn: Function, ...args: unknown[]) =>
+    new Promise((resolve) => enqueue(fn, resolve, args));
 
-      const res = await Promise.resolve(fn)
+  return generator;
+};
 
-      result[current] = res;
-      console.log('res: ', res);
-      if (current < len) {
-        next()
-      }
-    }
-  });
-}
+const delay = (timer: number) => new Promise((resolve) => setTimeout(resolve, timer));
 
-const fn = (timer: number):Promise<any> => sleep(timer).then(() => {
-  return Promise.resolve(timer)
-  console.log(222)
-});
-
-const testFn = () => 444
-
-const fns = [fn(1000), fn(2000), fn(3000), fn(1000), testFn(), fn(4000)];
-
-promiseLimit(fns, 2).then(res => {
-  console.log('res: ', res);
-})
+const limit = pLimit(1);

fa6f9c88f281e7456eb81cccd3ce52d4db45b5cb
ninenan <wuqingnan.wqn@digital-engine.com>
2023-06-09 14:18:38 +0800

    chore: 删除多余文件

--- src/type-challenges/add.ts
+++ src/type-challenges/add.ts
@@ -1,11 +1,11 @@
-type CreateTupple<
+type CreateTuple<
   S extends number,
   Res extends 1[] = []
-> = Res["length"] extends S ? Res : CreateTupple<S, [...Res, 1]>;
+> = Res["length"] extends S ? Res : CreateTuple<S, [...Res, 1]>;
 
 type Add<A extends number, B extends number> = [
-  ...CreateTupple<A>,
-  ...CreateTupple<B>
+  ...CreateTuple<A>,
+  ...CreateTuple<B>
 ]["length"];
 
 const num: Add<999, 99> = 1098;
